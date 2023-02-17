ff9b69815ed2179ad2a24be27b856ed90c9ea97f
Angelo Filho <angelo.filho06@hotmail.com>
2023-02-16 23:03:31 -0300

    storybook/feat: add button component

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,4 +1,35 @@
 import { colors } from '@angelino-ui/tokens'
+import { ComponentProps } from 'react'
+import { styled } from './styles'
+
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+  padding: '$2 $4',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
 
 export function App() {
   return <h1 style={{ color: colors.ignite300 }}>Hello World!</h1>
