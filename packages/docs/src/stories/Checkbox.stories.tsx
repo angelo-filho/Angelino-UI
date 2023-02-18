8021c04817cf07135cbe8a984f92abaa69859d97
Angelo Filho <angelo.filho06@hotmail.com>
2023-02-18 07:17:49 -0300

    feat(react/storybook): add checkbox component

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,28 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Checkbox, CheckboxProps, Text } from '@angelino-ui/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+
+  args: {
+    src: 'https://github.com/angelo-filho.png',
+    alt: 'Angelo Filho',
+  },
+
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', alignItems: 'center', gap: '$2' }}
+        >
+          {Story()}
+          <Text size={'sm'}>Accept terms of use</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {}
